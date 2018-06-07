8aaf9c7651b1aeeefcffb96386c05854cace5d53
Allora Sisnroy <allsis@icloud.com>
2018-06-07 16:02:28 +0000

    automatically backed up by learn

--- silly_strings.rb
+++ silly_strings.rb
@@ -2,4 +2,4 @@ puts "IM NOT SHOUTING".downcase
 puts "supercalifragilisticexpialadocious".length 
 puts "wow" + "mom"
 puts "i really like programing".upcase 
-puts ""
+puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
